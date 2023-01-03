e453cae174868c88757882114a73bafc61cb2f96
Raja <111275397+Mr-Raja12@users.noreply.github.com>
2023-01-04 00:45:31 +0530

    Added : Write a program to demonstrate File Operations like

--- src/com/bridgelabz/EmployeePayrollService.java
+++ src/com/bridgelabz/EmployeePayrollService.java
@@ -10,35 +10,31 @@ import java.util.Scanner;
  */
 public class EmployeePayrollService {
 
-    private List<EmployeePayrollData> employeePayrollList;//declare variable
+	private List<EmployeePayrollData> employeePayrollList;
 
-    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {//Parameterized constructor
-        this.employeePayrollList = employeePayrollList;
-    }
+	public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+		this.employeePayrollList = employeePayrollList;
+	}
 
-    public static void main(String[] args) {
-        //create Arraylist which is of employee payroll data type
-        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-        //create object of employee payroll service class using new keyword
-        EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);//constructor
-        Scanner consoleInputReader = new Scanner(System.in);
-        employeePayrollService.readEmployeePayrollData(consoleInputReader);//calling read method using obj reference
-        employeePayrollService.writeEmployeePayrollData();//calling write method using obj reference
-    }
+	public static void main(String[] args) {
+		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData();
+	}
 
-    // Create the Read Employee Payroll Data Method
-    private void readEmployeePayrollData(Scanner consoleInputReader) {
-        System.out.println("Enter Employee ID: ");
-        int id = consoleInputReader.nextInt();
-        System.out.println("Enter Employee Name: ");
-        String name = consoleInputReader.next();
-        System.out.println("Enter Employee Salary: ");
-        double salary = consoleInputReader.nextDouble();
-        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
-    }
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("Enter Employee ID: ");
+		int id = consoleInputReader.nextInt();
+		System.out.println("Enter Employee Name: ");
+		String name = consoleInputReader.next();
+		System.out.println("Enter Employee Salary: ");
+		double salary = consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+	}
 
-    //Create to Write Employee payroll Data Method and passing the message and print employeePayrollList
-    private void writeEmployeePayrollData() {
-        System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
-    }
+	private void writeEmployeePayrollData() {
+		System.out.println("\nWriting Employee Payroll Roaster to Console\n" + employeePayrollList);
+	}
 }
